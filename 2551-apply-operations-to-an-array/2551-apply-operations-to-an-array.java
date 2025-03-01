63d0dd1b4d4162b2a83dabbd9bdabf6184fabb56
Abhishek Kumar <119113763+Di-Abhi@users.noreply.github.com>
2025-03-01 23:11:48 +0530

    Time: 1 ms (96.37%), Space: 43.5 MB (92.81%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -1,34 +1,21 @@
 class Solution {
-    public void moveZeros(int[] nums){
-        int j=-1;
+    public int[] applyOperations(int[] nums) {
+        int nk=0;
         int n = nums.length;
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
-                j=i;
-                break;
-            }
-        }
-        if(j==-1) return;
-
-        for(int i = j+1;i<n;i++){
-            if(nums[i]!=0){
-                int tmp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=tmp;
-                j++;
-            }
-        }
-    }
-    public int[] applyOperations(int[] nums) {
-        int n = nums.length;
-
-        for(int i =0;i<n-1;i++){
-            if(nums[i]==nums[i+1]){
+            if(i<n-1 && nums[i]==nums[i+1] && nums[i]!=0){
                 nums[i]*=2;
                 nums[i+1]=0;
             }
+            if(nums[i]!=0){
+                if(i!=nk){
+                    int tmp = nums[i];
+                    nums[i]=nums[nk];
+                    nums[nk]=tmp;
+                }
+                nk++;
+            }
         }
-        moveZeros(nums);
         return nums;
     }
 }
