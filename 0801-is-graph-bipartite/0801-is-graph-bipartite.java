b13477ec6345845347b31647bf1424290324d7c0
Abhishek Kumar <119113763+Di-Abhi@users.noreply.github.com>
2025-09-25 00:36:33 +0530

    Time: 0 ms (100%), Space: 45.1 MB (67.68%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -1,20 +1,21 @@
 class Solution {
-    private boolean check(int start,int n,int[][] graph,int[] color){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        color[start]=0;
-        while(!q.isEmpty()){
-            int node = q.peek();
-            q.remove();
+    private boolean dfs(int node,int col,int[][] graph,int[] color){
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(start);
+        color[node]=col;
+        // while(!q.isEmpty()){
+        //     int node = q.peek();
+        //     q.remove();
             for(int it:graph[node]){
                 if(color[it]==-1){
-                    color[it]=1-color[node];
-                    q.add(it);
+                    // color[it]=1-color[node];
+                    // q.add(it);
+                    if(dfs(it,1-col,graph,color)==false) return false;
                 }else if(color[it]==color[node]){
                     return false;
                 }
             }
-        }
+        // }
         return true;
     }
     public boolean isBipartite(int[][] graph) {
@@ -23,7 +24,7 @@ class Solution {
         for(int i=0;i<n;i++) color[i]=-1;
         for(int i=0;i<n;i++){
             if(color[i]==-1){
-                if(check(i,n,graph,color)==false){
+                if(dfs(i,0,graph,color)==false){
                     return false;
                 }
             }
