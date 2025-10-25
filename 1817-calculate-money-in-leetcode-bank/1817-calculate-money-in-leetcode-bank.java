a6ee3b5f1697bc16e07646b151e6149efba4bc30
Abhishek Kumar <119113763+Di-Abhi@users.noreply.github.com>
2025-10-26 02:49:50 +0530

    Time: 1 ms (56.65%), Space: 40.6 MB (32.51%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.java
@@ -1,8 +1,6 @@
 class Solution {
     public int totalMoney(int n) {
         int money = 0;
-        int weeks = n/7;
-        int rmDays = n%7;
 
         for(int i=0;i<n/7;i++){
             for(int k=1;k<=7;k++){
