b053a15a67b6ace4f6fa2e1753e40365686d4296
Abhishek Kumar <119113763+Di-Abhi@users.noreply.github.com>
2026-01-11 17:28:49 +0530

    Time: 15 ms (59.33%), Space: 49.8 MB (44.09%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -3,30 +3,31 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         int height[] = new int[m];
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1'){
-                    height[j]+=1;
-                }else {
-                    height[j]=0;
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == '1') {
+                    height[j] += 1;
+                } else {
+                    height[j] = 0;
                 }
             }
-            maxArea=Math.max(maxArea,solve(height));
+            maxArea = Math.max(maxArea, solve(height));
         }
         return maxArea;
     }
-    public int solve(int heights[]){
-        int n=heights.length;
-        int maxArea=0;
+
+    public int solve(int heights[]) {
+        int n = heights.length;
+        int maxArea = 0;
         Stack<Integer> st = new Stack<>();
-        for(int i=0;i<=n;i++){
-            int curr = (i==n)?0:heights[i];
-            while(!st.isEmpty()&&curr<heights[st.peek()]){
-                int height=heights[st.pop()];
-                int left =(st.isEmpty()?-1:st.peek());
-                int width = i-left-1;
-                maxArea=Math.max(maxArea,height*width);
+        for (int i = 0; i <= n; i++) {
+            int curr = (i == n) ? 0 : heights[i];
+            while (!st.isEmpty() && curr < heights[st.peek()]) {
+                int height = heights[st.pop()];
+                int left = (st.isEmpty() ? -1 : st.peek());
+                int width = i - left - 1;
+                maxArea = Math.max(maxArea, height * width);
             }
             st.push(i);
         }
